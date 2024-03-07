a1cab4a9bdfc47c73d7a30c804a7f9780c0682ce
beardyface <henryamwilliams@gmail.com>
2024-03-07 17:16:04 +1300

    add coordinates

--- examples/ur_robot_env.py
+++ examples/ur_robot_env.py
@@ -20,19 +20,20 @@ class Env:
         self.robot.set_tcp((0, 0, 0, 0, 0, 0))  # Set tool central point
         self.robot.set_payload(0.5, (0, 0, 0))  # Kg
 
-    def home_position(self, joint_state):
-        #home_pose = (-1.600, -1.727, -2.202, -0.807, 1.595, -0.030) # Joint in rad for home position
-        self.robot.movej(joint_state, vel=self.vel, acc=self.acc, wait=True)
+    def home_position(self):
+        home_pose = (1.5956498, -1.47229, 1.4730, -1.60006, 4.688987, 0.07035151) # Joint in rad for home position
+        self.robot.movej(home_pose, vel=self.vel, acc=self.acc, wait=True)
         logging.info("Robot at home")
+
 
     def read_joint_state(self):
         joint_state = self.robot.getj()
         return joint_state
 
-    # def move_rest_position(self):
-    #     self.robot.movel
-
-
+    def move_reset_position(self):
+        reset_pose = (1.595745682, -1.5322321, 1.4995, -3.11227, 4.724702, 0.0004789)
+        self.robot.movej(reset_pose, vel=self.vel, acc=self.acc, wait=True)
+        logging.info("Robot at reset pose")
 
 
 
@@ -47,13 +48,15 @@ def main():
     config = read_config_file()
     ip_address_robot = config['ip_robot']
 
-    robot = urx.Robot(ip_address_robot, use_rt=True, urFirm=config['urFirm'])
+    # robot = urx.Robot(ip_address_robot, use_rt=True, urFirm=config['urFirm'])
+    robot = urx.Robot(ip_address_robot)
+    
+
     robot_env = Env(robot)
-
     joints = robot_env.read_joint_state()
-    print(joints)
 
-    #robot_env.home_position()
+    robot_env.home_position()
+    robot_env.move_reset_position()
 
     robot.close()
 
