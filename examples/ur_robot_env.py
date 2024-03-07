9ee8813406b1ffe06eb746ea72e9f0a3f22274d5
david <davidvalenciaredrovan@gmail.com>
2024-03-07 17:41:12 +1300

    add class

--- examples/ur_robot_env.py
+++ examples/ur_robot_env.py
@@ -20,20 +20,23 @@ class Env:
         self.robot.set_tcp((0, 0, 0, 0, 0, 0))  # Set tool central point
         self.robot.set_payload(0.5, (0, 0, 0))  # Kg
 
+    def read_joint_state(self):
+        joint_state = self.robot.getj()
+        return joint_state
+
+
     def home_position(self):
         home_pose = (1.5956498, -1.47229, 1.4730, -1.60006, 4.688987, 0.07035151) # Joint in rad for home position
         self.robot.movej(home_pose, vel=self.vel, acc=self.acc, wait=True)
         logging.info("Robot at home")
 
-
-    def read_joint_state(self):
-        joint_state = self.robot.getj()
-        return joint_state
-
     def move_reset_position(self):
         reset_pose = (1.595745682, -1.5322321, 1.4995, -3.11227, 4.724702, 0.0004789)
         self.robot.movej(reset_pose, vel=self.vel, acc=self.acc, wait=True)
         logging.info("Robot at reset pose")
+
+    def move_test(self):
+
 
 
 
@@ -50,7 +53,7 @@ def main():
 
     # robot = urx.Robot(ip_address_robot, use_rt=True, urFirm=config['urFirm'])
     robot = urx.Robot(ip_address_robot)
-    
+
 
     robot_env = Env(robot)
     joints = robot_env.read_joint_state()
